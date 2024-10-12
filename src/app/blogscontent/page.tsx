8c360ead64e30ccca87fabc4bdced6f7e43c117a
Wajeeha02 <wajeehausman9832@gmail.com>
2024-10-12 22:52:44 +0500

    few changings

---
+++ src/app/blogscontent/page.tsx
@@ -0,0 +1,16 @@
+
+import Blog from '../components/BlogsContent/Blogs';
+import Footer from "../components/LandingPage/Footer";
+import Header from "../components/LandingPage/Header";
+const page = () => {
+  return (
+    <>
+      {/** */}
+      <Header />
+      <Blog/>
+      <Footer />
+    </>
+  );
+};
+
+export default page;
